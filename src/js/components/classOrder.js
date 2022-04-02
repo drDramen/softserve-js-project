615aac4bbaa34007b0fc33e849de7fecd518e40c
drDramen <vvpalchin@gmail.com>
2022-04-02 15:18:32 +0300

    init gh-pages

--- src/js/components/classOrder.js
+++
@@ -1,7 +0,0 @@
-class Order {
-  constructor(products, totalSum) {
-    this.date = new Date();
-    this.sum = totalSum;
-    this.products = products;
-  }
-}
--- src/js/components/orders.js
+++
@@ -1,6 +0,0 @@
-const ordersLink = document.querySelector('.orders-link');
-const ordersBody = document.querySelector('orders-items body');
-
-function renderOrders() {
-
-}
